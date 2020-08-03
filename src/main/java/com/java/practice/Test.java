18ee9f898054ef82a1e922228981f8805a6bdaa2
ACC-1 <ACC-1@ACC-1.it.regentdom>
2020-08-03 09:28:24 +0530

    test file

---
+++ src/main/java/com/java/practice/Test.java
@@ -0,0 +1,24 @@
+package com.java.practice;
+
+public class Test{
+	
+	public void methodTest() {
+		
+	}
+	
+}
+
+class TestImpl{
+	
+	Test test;
+	public TestImpl(){
+	
+	}
+	
+	
+}
+
+class SampleA{
+	
+	
+}
